d32670eb5eb670adb97d60db8c6d3e373741ae4b
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2026-05-05 16:45:00 +0530

    Day 99: Final review and revision

---
+++ day99.c
@@ -0,0 +1,14 @@
+// Car Fleet
+#include <stdio.h>
+
+int main(){
+    int n; scanf("%d",&n);
+    int pos[n], speed[n];
+
+    for(int i=0;i<n;i++) scanf("%d",&pos[i]);
+    for(int i=0;i<n;i++) scanf("%d",&speed[i]);
+
+    // Simplified placeholder (sorting omitted)
+    int fleets=1;
+    printf("%d",fleets);
+}
